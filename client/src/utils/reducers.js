d743796631dcfba5b15dcc704912db09195f2be3
trujilml <mickeytrj@gmail.com>
2022-02-23 10:49:18 -0600

    found myself working on the reducers file by mistake in the client directory - back to work on app.js - mekayla

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,14 +1,43 @@
-import { LOAD_WORKOUTS,
-  LOAD_FRIENDS,
-  ADD_WORKOUT,
-  UPDATE_WORKOUT,
-  REMOVE_WORKOUT,
-  ADD_FRIEND,
-  REMOVE_FRIEND
-  }  from "./actions";
 import { useReducer } from 'react';
 
-// export const reducer = (state, action) => {
-//   switch (action.type)
+import { 
+  LOAD_WORKOUTS,
+  LOAD_FRIENDS,
+  ADD_WORKOUT,
+  // UPDATE_WORKOUT,
+  // REMOVE_WORKOUT,
+  // ADD_FRIEND,
+  // REMOVE_FRIEND
+  }  from "./actions";
 
-// }
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case LOAD_WORKOUTS:
+      return {
+        ...state,
+        workouts: [...action.workouts],
+      };
+
+    case LOAD_FRIENDS:
+      return {
+        ...state,
+        friends: [...action.friends],
+      };
+
+      case ADD_WORKOUT:
+        return {
+          ...state,
+          workoutOpen: true,
+          workout: [...state.workout, action.workout]
+        }
+
+  default:
+    return state;
+
+}
+};
+
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
