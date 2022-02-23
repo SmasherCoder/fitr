ca7e7bc6554d8c8ebd5b8ace264ed40ce2cfad63
trujilml <mickeytrj@gmail.com>
2022-02-23 13:15:24 -0600

    restructed nav component removed header - now to figure out site design again with new format

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
-// import './App.css';
-import Header from './components/Header';
+import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// import Header from './components/Header';
+import Navigation from "./components/Nav";
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import NoMatch from './pages/NoMatch';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+
+  uri: "/graphql",
+});
 
 function App() {
   return (
-    <div className = "app">
-      <Header />
-    </div>
+    <ApolloProvider client = {client}>
+      <Router>
+        <>
+        <Navigation />
+        <div className='container'>
+          <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} /> 
+  
+        <Route component={NoMatch} /> 
+        </Switch>
+        </div>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,39 +1,76 @@
 // Placeholder Header Index file
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import Login from '../../pages/Login';
+import Signup from '../../pages/Signup';
+
 import Auth from '../../utils/auth';
 
-const Header = () => {
-  const logout = event => {
-    event.preventDefault();
-    Auth.logout();
-  };
+const Navigation = () => {
+
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
-          <h1>Fitr</h1>
-        </Link>
+    <>
+    <Navbar bg="light" variant="light" expand="xl">
+      <Container fluid>
+        <Navbar.Brand as={Link} to='/'>
+          Fitr
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbar' />
+        <Navbar.Collapse id='navbar'>
+          <Nav className='ml-auto'>
+            <Nav.Link as={Link} to='/'>
+              View Workouts/Friends
+              </Nav.Link>
+              {Auth.loggedIn() ? (
+                <>
+                <Nav.Link as={Link} to='/follow'>
+                  View Followed Friends
+                </Nav.Link>
+                <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                </>
+              ) : (
+                <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+              )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 
-        <nav className="text-center">
-          {Auth.loggedIn() ? (
-            <>
-              <Link to="/profile">Me</Link>
-              <a href="/" onClick={logout}>
-                Logout
-              </a>
-            </>
-          ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </>
-          )}
-        </nav>
-       </div>
-    </header>
+    <Modal
+    size='xl'
+    show={showModal}
+    onHide={() => setShowModal(false)}
+    aria-labelledby='signup-modal'>
+      <Tab.Container defaultActiveKey='login'>
+        <Modal.Header closeButton>
+          <Modal.Title id='signup-modal'>
+            <Nav variant='pills'>
+              <Nav.Item>
+                <Nav.Link eventKey='login'></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                </Nav.Item>
+            </Nav>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Tab.Content>
+          <Tab.Pane eventKey='login'>
+                <Login handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey='signup'>
+                <Signup handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+          </Tab.Content>
+        </Modal.Body>
+      </Tab.Container>
+    </Modal>
+    </>
   );
 };
 
-export default Header;
+export default Navigation;
