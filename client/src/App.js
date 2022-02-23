7777e54c451c91b3a2b345313fbad98908a36c38
trujilml <mickeytrj@gmail.com>
2022-02-23 13:52:26 -0600

    developed css for the main app and nav component now to figure out other components to show workouts/friends with queries

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// import Header from './components/Header';
 import Navigation from "./components/Nav";
-
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,4 +1,3 @@
-// Placeholder Header Index file
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
@@ -13,17 +12,21 @@ const Navigation = () => {
 
   return (
     <>
-    <Navbar bg="light" variant="light" expand="xl">
+    <Navbar bg="light" variant="light" expand="lg">
       <Container fluid>
         <Navbar.Brand as={Link} to='/'>
           Fitr
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='navbar' />
         <Navbar.Collapse id='navbar'>
           <Nav className='ml-auto'>
             <Nav.Link as={Link} to='/'>
-              View Workouts/Friends
+              What Others Are Doing
               </Nav.Link>
+              <br></br>
+              <Nav.Link as={Link} to='/'>
+                About Fitr
+              </Nav.Link>
+              <br></br>
               {Auth.loggedIn() ? (
                 <>
                 <Nav.Link as={Link} to='/follow'>
@@ -40,23 +43,11 @@ const Navigation = () => {
     </Navbar>
 
     <Modal
-    size='xl'
+    size='lg'
     show={showModal}
     onHide={() => setShowModal(false)}
     aria-labelledby='signup-modal'>
       <Tab.Container defaultActiveKey='login'>
-        <Modal.Header closeButton>
-          <Modal.Title id='signup-modal'>
-            <Nav variant='pills'>
-              <Nav.Item>
-                <Nav.Link eventKey='login'></Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
-            </Nav>
-          </Modal.Title>
-        </Modal.Header>
         <Modal.Body>
           <Tab.Content>
           <Tab.Pane eventKey='login'>
