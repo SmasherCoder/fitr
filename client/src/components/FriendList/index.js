6328f15164104e93fb59ed0da18debfe6b8678e2
Dustin <dustin2400@hotmail.com>
2022-03-01 01:56:41 -0600

    fixed last minute bugs

--- client/src/components/FriendList/index.js
+++ client/src/components/FriendList/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const FriendList = ({ friendCount, username, friends}) => {
-    if (!friends || !friends.length) { return <p className="bg-dark text-light p-3"> {username}, make some friends! </p>;
+const FriendList = ({ followCount, username, follow}) => {
+    if (!follow || !follow.length) { return <p className="bg-dark text-light p-3"> {username}, make some friends! </p>;
 }
 
 return (
-    <div>
+    <div className="friend-list">
         <h5>
-            {username}'s {friendCount} {friendCount === 1 ? 'friend' : 'friends'}
+            {username}'s {followCount} {followCount === 1 ? 'friend' : 'friends'}
         </h5>
-        {friends.map(friend => (
+        {follow.map(friend => (
             <button className = "btn w-100 display-block mb-2" key={friend._id}>
                 <Link to={`/profile/${friend.username}`}>{friend.username}</Link>
             </button>
